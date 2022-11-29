538d32363d3b86928c332908c60a996804f304ba
RahulPalXDA <RahulPal.XDA@gmail.com>
2022-11-29 07:16:04 +0000

    G: Initial rebrand

--- lineage_G.mk
+++ lineage_G.mk
@@ -18,25 +18,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from mido device
-$(call inherit-product, device/xiaomi/mido/device.mk)
+# Inherit from G device
+$(call inherit-product, device/10or/G/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
-PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 4
-PRODUCT_MANUFACTURER := Xiaomi
-TARGET_VENDOR := Xiaomi
-BOARD_VENDOR := Xiaomi
+PRODUCT_DEVICE := G
+PRODUCT_NAME := lineage_G
+PRODUCT_BRAND := 10or
+PRODUCT_MODEL := 10or G
+PRODUCT_MANUFACTURER := 10or
+TARGET_VENDOR := 10or
+BOARD_VENDOR := 10or
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
+PRODUCT_GMS_CLIENTID_BASE := android-10or
