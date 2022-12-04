28beef5ab420ada98e34694b416f363df4c93e5c
RahulPalXDA <RahulPal.XDA@gmail.com>
2022-12-04 12:42:06 +0530

    G: drop mido camera

--- device.mk
+++ device.mk
@@ -157,9 +157,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.5:64 \
     android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
-    camera.msm8953 \
     libcamshim \
-    libmm-qcamera \
     libui_shim
 
 # Cgroup and task_profiles
